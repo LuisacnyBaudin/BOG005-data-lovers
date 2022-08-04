75972a99ede8da5b659cfae9a67a71fd3b928c35
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-08-04 12:03:09 -0500

    prueba

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const housesGryffindor= (data) => {
+  return data.characters.filter(character => character.house && character.house.includes("Gryffindor"));
+}
+export const housesSlytherin= (data) => {
+  return data.characters.filter(character => character.house && character.house.includes("Slytherin"));
+}
+export const housesRavenclaw= (data) => {
+  return data.characters.filter(character => character.house && character.house.includes("Ravenclaw"));
+}
+export const housesHufflepuff= (data) => {
+  return data.characters.filter(character => character.house && character.house.includes("Hufflepuff"));
+}
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+import data from './data/harrypotter/data.js';
+import {housesGryffindor} from './data.js';
+const filteredDatag= housesGryffindor (data);
+
+import {housesSlytherin} from './data.js';
+const filteredDataS= housesSlytherin (data);
+
+import {housesRavenclaw} from './data.js';
+const filteredDataR= housesRavenclaw (data);
+
+import {housesHufflepuff} from './data.js';
+const filteredDataH= housesHufflepuff (data);
+
+console.log ( filteredDatag,filteredDataS, filteredDataR, filteredDataH );
